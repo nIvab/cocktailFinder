6e8d72f3d00137fad7f0261ff9bb6ad9be3fce51
Ivan B <ivanbolderoff@gmail.com>
2021-01-29 15:45:31 +1030

    fixed bug where site will crash if user enters combination of ingredients that no drinks use

--- src/Components/Card/CardList.js
+++ src/Components/Card/CardList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion, AnimateSharedLayout } from "framer-motion";
 
+import "./CardList.css";
 import Card from "./Card";
 /* the input for cardlist, list has the following structure 
 {
@@ -15,19 +16,20 @@ import Card from "./Card";
 function CardList({ list }) {
     // generates cards of appropriate drinks from list given by API call
     console.log("LIST:  ", list);
-    if (list === "None Found" || list.drinks == []) {
-        return <h1>Sorry we could find any drinks</h1>;
+    if (list.drinks === "None Found" || list.drinks == []) {
+        return <h1 className="NoDrinks">Sorry we could find any drinks</h1>;
+    } else {
+        return (
+            <AnimateSharedLayout>
+                <motion.div layout initial={{ borderRadius: 25 }}>
+                    {list.drinks.map((listItem) => {
+                        // map each drink to its own card
+                        <Card key={listItem} />;
+                    })}
+                </motion.div>
+            </AnimateSharedLayout>
+        );
     }
-    return (
-        <AnimateSharedLayout>
-            <motion.div layout initial={{ borderRadius: 25 }}>
-                {list.drinks.map((listItem) => {
-                    // map each drink to its own card
-                    <Card key={listItem} />;
-                })}
-            </motion.div>
-        </AnimateSharedLayout>
-    );
 }
 
 export default CardList;
